7f73141b72efcfa16bbbf06e820d6d288eb24704
Jakob Malm <jakob.malm@gmail.com>
2024-07-10 16:03:19 +0200

    Create schedule for visualization

--- on_percentage.py
+++ on_percentage.py
@@ -7,12 +7,19 @@ class OnByPercentage(hass.Hass):
     Turn on a device a given percentage of the time, when the electricity price is the lowest, or always,
     when the electricity price is below a given threshold.
     """
+    percentage = 50
+    price_entity_id = None
+    price_threshold = 0.0
+    device = None
+    schedule_entity_id = None
+
     def initialize(self):
-        self.device_entity_id = str(self.args['device_entity_id'])
+        device_entity_id = str(self.args['device_entity_id'])
+        self.device = self.get_entity(device_entity_id)
         self.percentage = int(self.args['percentage'])
         self.price_entity_id = str(self.args['price_entity_id'])
         self.price_threshold = float(self.args['price_threshold'])
-        self.device = self.get_entity(self.device_entity_id)
+        self.schedule_entity_id = str(self.args['schedule_entity_id'])
 
         self.run_hourly(self.set_suitable_state, datetime.time(0, 1, 0))
         self.set_suitable_state()
@@ -26,19 +33,57 @@ class OnByPercentage(hass.Hass):
         possible_hours = today + tomorrow
         prices = [float(h['value']) for h in possible_hours]
 
-        if current_price <= self.price_threshold or price_is_among_lowest(current_price, prices, self.percentage):
-            if self.device.state == "off":
-                self.log(f"Electricity price is {current_price} {currency}/kWh which is among the lowest {self.percentage} % of the hourly prices. Turning {self.device.friendly_name} on.")
-                self.turn_on(self.device_entity_id)
+        # Should switch be on or off?
+        on, reason = self.should_be_on(current_price, prices)
+        target_state = 'on' if on else 'off'
+
+        # Create the schedule (for visualization).
+        should_be_on_hours = [h for h in possible_hours if self.should_be_on(h['value'], prices)[0]]
+        schedule = get_contiguous_slots(should_be_on_hours)
+        self.log(f"Schedule: {schedule}")
+        self.set_state(self.schedule_entity_id, state=target_state, attributes={"schedule": schedule}, replace=True)
+
+        # Turn on or off, when needed.
+        change_state = self.device.state != target_state
+        log_level = "INFO" if change_state else "DEBUG"
+        message = f"Electricity price is {current_price} {currency}/kWh which is {reason}."
+        if change_state:
+            message += f" Turning {self.device.friendly_name} {target_state}."
         else:
-            if self.device.state == "on":
-                self.log(f"Electricity price is {current_price} {currency}/kWh which is NOT among the lowest {self.percentage} % of the hourly prices. Turning {self.device.friendly_name} off.")
-                self.turn_off(self.device_entity_id)
+            message += f" {self.device.friendly_name} is already {target_state}."
+        self.log(message, level=log_level)
+        self.set_state(self.device.entity_id, state=target_state, attributes={"reason": reason, "schedule": schedule},
+                       replace=True)
+
+    def should_be_on(self, price, prices):
+        if price <= self.price_threshold:
+            return True, "lower than threshold ({self.price_threshold})"
+        if price_is_among_lowest(price, prices, self.percentage):
+            return True, "among the lowest {self.percentage} % of the hourly prices"
+        return False, "NOT among the lowest {self.percentage} % of the hourly prices"
 
 
-def price_is_among_lowest(current_price : float, prices : list[float], limit : float) -> bool:
+def get_contiguous_slots(slots: list[dict[str, datetime]]) -> list[dict[str, datetime]]:
+    """Get the contiguous slots of the given prices."""
+    sorted_slots = sorted(slots, key=lambda x: x['start'])
+
+    def start_end(s: dict[str, datetime]) -> dict[str, datetime]:
+        return {'start': s['start'], 'end': s['end']}
+
+    contiguous_slots = []
+    for slot in sorted_slots:
+        if len(contiguous_slots) == 0:
+            contiguous_slots.append(start_end(slot))
+        elif contiguous_slots[-1]['end'] == slot['start']:
+            contiguous_slots[-1]['end'] = slot['end']
+        else:
+            contiguous_slots.append(start_end(slot))
+    return contiguous_slots
+
+
+def price_is_among_lowest(current_price: float, prices: list[float], percentage: float) -> bool:
     """
-    Return true when *current_price* is within the *limit* % lowest *prices*.
+    Return true when *current_price* is within the *percentage* % lowest *prices*.
     False otherwise.
 
     Example:
@@ -51,4 +96,4 @@ def price_is_among_lowest(current_price : float, prices : list[float], limit : f
     """
     from bisect import bisect
     index = bisect(sorted(prices), current_price)
-    return index / len(prices) <= limit / 100
+    return index / len(prices) <= percentage / 100
