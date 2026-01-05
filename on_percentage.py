e5dc3c317ba22615798e7086f913538a3d12df9f
Jakob Malm <jakob.malm@gmail.com>
2026-01-05 23:13:53 +0100

    Add optional percentage entity setting

--- on_percentage.py
+++ on_percentage.py
@@ -6,23 +6,49 @@ class OnByPercentage(hass.Hass):
     """
     Turn on a device a given percentage of the time, when the electricity price is the lowest, or always,
     when the electricity price is below a given threshold.
+
+    Args:
+        device_entity_id: The entity ID of the device to control.
+        percentage: (Optional) Initial percentage value (0-100). Defaults to 50. Can be overridden by percentage_entity_id.
+        price_entity_id: The entity ID of the electricity price sensor.
+        price_threshold: Price threshold below which the device is always on.
+        schedule_entity_id: Entity ID for the schedule visualization.
+        percentage_entity_id: (Optional) Entity ID of an input_number for dynamic percentage control.
     """
     percentage = 50
     price_entity_id = None
     price_threshold = 0.0
     device = None
     schedule_entity_id = None
+    percentage_entity_id = None
 
     def initialize(self):
         device_entity_id = str(self.args['device_entity_id'])
         self.device = self.get_entity(device_entity_id)
-        self.percentage = int(self.args['percentage'])
+        self.percentage = int(self.args.get('percentage', 50))
         self.price_entity_id = str(self.args['price_entity_id'])
         self.price_threshold = float(self.args['price_threshold'])
         self.schedule_entity_id = str(self.args['schedule_entity_id'])
+        self.percentage_entity_id = self.args.get('percentage_entity_id')
+
+        if self.percentage_entity_id:
+            self.listen_state(self.percentage_changed, self.percentage_entity_id)
+            current_percentage = self.get_state(self.percentage_entity_id)
+            if current_percentage is not None:
+                self.percentage = int(float(current_percentage))
+            else:
+                # Set initial value if entity exists but no state
+                self.set_state(self.percentage_entity_id, state=self.percentage)
 
         self.run_hourly(self.set_suitable_state, datetime.time(0, 1, 0))
         self.set_suitable_state()
+
+    def percentage_changed(self, entity, attribute, old, new, kwargs):
+        if new is not None:
+            self.percentage = int(float(new))
+            self.log(f"Percentage updated to {self.percentage}")
+            # Optionally, re-evaluate the state immediately
+            self.set_suitable_state()
 
     def set_suitable_state(self, *args, **kwargs):
         state = self.get_state(self.price_entity_id, attribute="all")
